5625801dcf8b8031c4a6fa7d728874f87a9e7369
Gabriel Guerrero <gabriel.issa@medivet.co.uk>
2021-07-23 23:03:00 +0100

    fix: remove dependency on angular/cdk

--- libs/ngrx-traits/traits/src/sort/sort.utils.ts
+++ libs/ngrx-traits/traits/src/sort/sort.utils.ts
@@ -1,7 +1,14 @@
-import { _isNumberValue } from '@angular/cdk/coercion';
 import { Sort } from './sort.model';
 
 const MAX_SAFE_INTEGER = 9007199254740991;
+
+export function _isNumberValue(value: any): boolean {
+  // parseFloat(value) handles most of the cases we're interested in (it treats null, empty string,
+  // and other non-number values as NaN, where Number just uses 0) but it considers the string
+  // '123hello' to be a valid number. Therefore we also check if Number(value) is NaN.
+  return !isNaN(parseFloat(value as any)) && !isNaN(Number(value));
+}
+
 function sortingDataAccessor<T>(
   data: T,
   sortHeaderId: string
