a7e4bf2cd7ae201c81ab3717db809f524fceed53
Gabriel Guerrero <gabriel.issa@medivet.co.uk>
2022-03-31 12:18:32 +0100

    docs(local-traits): fix missing local-traits docs
    Added the need to override the constructor and all this.traits.addEffects(this) if effects are added
    to the LocalTraits service

--- apps/example-app/src/app/examples/product-picker-page/components/product-select-dialog/product-select-dialog.component.ts
+++ apps/example-app/src/app/examples/product-picker-page/components/product-select-dialog/product-select-dialog.component.ts
@@ -4,7 +4,7 @@ import { map } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 import { Product, ProductFilter } from '../../../models';
 import { Sort } from 'ngrx-traits/traits';
-import { ProductsLocalTraits } from './products.traits';
+import { ProductsLocalTraits } from './products.local-traits';
 
 @Component({
   selector: 'product-select-dialog',
@@ -47,9 +47,9 @@ import { ProductsLocalTraits } from './products.traits';
 })
 export class ProductSelectDialogComponent implements OnInit {
   data$ = combineLatest([
-    this.store.select(this.localTraits.localSelectors.selectProductsList),
-    this.store.select(this.localTraits.localSelectors.isProductsLoading),
-    this.store.select(this.localTraits.localSelectors.selectProductSelected),
+    this.store.select(this.traits.localSelectors.selectProductsList),
+    this.store.select(this.traits.localSelectors.isProductsLoading),
+    this.store.select(this.traits.localSelectors.selectProductSelected),
   ]).pipe(
     map(([products, isLoading, selectedProduct]) => ({
       products,
@@ -58,22 +58,20 @@ export class ProductSelectDialogComponent implements OnInit {
     }))
   );
 
-  constructor(private store: Store, private localTraits: ProductsLocalTraits) {}
+  constructor(private store: Store, private traits: ProductsLocalTraits) {}
 
   ngOnInit() {
-    this.store.dispatch(this.localTraits.localActions.loadProducts());
+    this.store.dispatch(this.traits.localActions.loadProducts());
   }
 
   select({ id }: Product) {
-    this.store.dispatch(this.localTraits.localActions.selectProduct({ id }));
+    this.store.dispatch(this.traits.localActions.selectProduct({ id }));
   }
 
   filter(filters: ProductFilter) {
-    this.store.dispatch(
-      this.localTraits.localActions.filterProducts({ filters })
-    );
+    this.store.dispatch(this.traits.localActions.filterProducts({ filters }));
   }
   sort(sort: Sort<Product>) {
-    this.store.dispatch(this.localTraits.localActions.sortProducts(sort));
+    this.store.dispatch(this.traits.localActions.sortProducts(sort));
   }
 }
