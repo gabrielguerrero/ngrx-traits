6138fd151f9afd6ff021d351222fdad082a6fccf
Gabriel Guerrero <gabriel.issa@medivet.co.uk>
2024-04-17 23:24:20 +0100

    fix: unit test, and small bug fixes found in unit test
    Missing unit test, and fixed bug in entitiesSelected() of withEntitiesMultiSelection

---
+++ libs/ngrx-traits/signals/src/lib/with-entities-selection/with-entities-single-selection.spec.ts
@@ -0,0 +1,71 @@
+import { withEntitiesSingleSelection } from '@ngrx-traits/signals';
+import { patchState, signalStore, type } from '@ngrx/signals';
+import { setAllEntities, withEntities } from '@ngrx/signals/entities';
+
+import { mockProducts } from '../test.mocks';
+import { Product } from '../test.model';
+
+describe('withEntitiesSingleSelection', () => {
+  const entity = type<Product>();
+
+  describe('without collection', () => {
+    const Store = signalStore(
+      withEntities({ entity }),
+      withEntitiesSingleSelection({ entity }),
+    );
+    it('selectEntity should select the entity', () => {
+      const store = new Store();
+      patchState(store, setAllEntities(mockProducts));
+      store.selectEntity({ id: mockProducts[4].id });
+      expect(store.entitiesSelectedEntity()).toEqual(mockProducts[4]);
+    });
+
+    it('deselectEntity should deselect the entity', () => {
+      const store = new Store();
+      patchState(store, setAllEntities(mockProducts));
+      store.selectEntity({ id: mockProducts[4].id });
+      store.deselectEntity({ id: mockProducts[4].id });
+      expect(store.entitiesSelectedEntity()).toEqual(undefined);
+    });
+
+    it('toggleEntity should toggle selection of the entity', () => {
+      const store = new Store();
+      patchState(store, setAllEntities(mockProducts));
+      store.toggleEntity({ id: mockProducts[4].id });
+      expect(store.entitiesSelectedEntity()).toEqual(mockProducts[4]);
+      store.toggleEntity({ id: mockProducts[4].id });
+      expect(store.entitiesSelectedEntity()).toEqual(undefined);
+    });
+  });
+
+  describe('with collection', () => {
+    const collection = 'products';
+    const Store = signalStore(
+      withEntities({ entity, collection }),
+      withEntitiesSingleSelection({ entity, collection }),
+    );
+    it('selectEntity should select the entity', () => {
+      const store = new Store();
+      patchState(store, setAllEntities(mockProducts, { collection }));
+      store.selectProductsEntity({ id: mockProducts[4].id });
+      expect(store.productsSelectedEntity()).toEqual(mockProducts[4]);
+    });
+
+    it('deselectEntity should deselect the entity', () => {
+      const store = new Store();
+      patchState(store, setAllEntities(mockProducts, { collection }));
+      store.selectProductsEntity({ id: mockProducts[4].id });
+      store.deselectProductsEntity({ id: mockProducts[4].id });
+      expect(store.productsSelectedEntity()).toEqual(undefined);
+    });
+
+    it('toggleEntity should toggle selection of the entity', () => {
+      const store = new Store();
+      patchState(store, setAllEntities(mockProducts, { collection }));
+      store.toggleProductsEntity({ id: mockProducts[4].id });
+      expect(store.productsSelectedEntity()).toEqual(mockProducts[4]);
+      store.toggleProductsEntity({ id: mockProducts[4].id });
+      expect(store.productsSelectedEntity()).toEqual(undefined);
+    });
+  });
+});
