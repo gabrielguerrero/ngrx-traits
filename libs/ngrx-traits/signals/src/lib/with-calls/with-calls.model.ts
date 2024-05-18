5d3cc51825b4f5747743ae2a7ccf9fb99eb175ee
Gabriel Guerrero <gabriel.issa@medivet.co.uk>
2024-05-18 09:10:23 +0100

    feat(signals): add option to type errors for withCalls, withCallStatus and withEntitiesLoadingCall
    Changes to allow typing the error withCalls, withCallStatus and withEntitiesLoadingCall, also a new
    mapError to allow transforming the error before storing it
    
    Fix #77

--- libs/ngrx-traits/signals/src/lib/with-calls/with-calls.model.ts
+++ libs/ngrx-traits/signals/src/lib/with-calls/with-calls.model.ts
@@ -1,3 +1,4 @@
+import { Signal } from '@angular/core';
 import { Observable } from 'rxjs';
 
 export type Call<Params extends readonly any[] = any[], Result = any> = (
@@ -7,13 +8,15 @@ export type CallConfig<
   Params extends readonly any[] = any[],
   Result = any,
   PropName extends string = string,
+  Error = any,
 > = {
   call: Call<Params, Result>;
   resultProp: PropName;
   mapPipe?: 'switchMap' | 'concatMap' | 'exhaustMap';
   storeResult?: boolean;
-  onSuccess?: (result: Result, ...params: Params) => void;
-  onError?: (error: unknown, ...params: Params) => void;
+  onSuccess?: (result: Result, param: Params[0]) => void;
+  mapError?: (error: unknown, param: Params[0]) => Error;
+  onError?: (error: Error, param: Params[0]) => void;
 };
 export type ExtractCallResultType<T extends Call | CallConfig> =
   T extends Call<any, infer R>
@@ -23,3 +26,21 @@ export type ExtractCallResultType<T extends Call | CallConfig> =
       : never;
 export type ExtractCallParams<T extends Call | CallConfig> =
   T extends Call<infer P> ? P : T extends CallConfig<infer P> ? P : [];
+
+export type NamedCallsStatusComputed<Prop extends string> = {
+  [K in Prop as `is${Capitalize<string & K>}Loading`]: Signal<boolean>;
+} & {
+  [K in Prop as `is${Capitalize<string & K>}Loaded`]: Signal<boolean>;
+};
+export type NamedCallsStatusErrorComputed<
+  Calls extends Record<string, Call | CallConfig>,
+> = {
+  [K in keyof Calls as `${K & string}Error`]: Calls[K] extends CallConfig<
+    any,
+    any,
+    any,
+    infer Error
+  >
+    ? Signal<Error | undefined>
+    : Signal<unknown | undefined>;
+};
