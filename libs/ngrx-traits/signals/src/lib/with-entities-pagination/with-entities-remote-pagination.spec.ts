48778c4bf883a5a5c1654f9487d171c57340b766
Gabriel Guerrero <gabriel.issa@medivet.co.uk>
2024-05-21 16:34:55 +0100

    fix(signals): fix NamedSetEntitiesResult not using correct name for setEntitiesPagedResult

--- libs/ngrx-traits/signals/src/lib/with-entities-pagination/with-entities-remote-pagination.spec.ts
+++ libs/ngrx-traits/signals/src/lib/with-entities-pagination/with-entities-remote-pagination.spec.ts
@@ -1,5 +1,6 @@
+import { effect, Signal, untracked } from '@angular/core';
 import { fakeAsync, TestBed, tick } from '@angular/core/testing';
-import { signalStore, type } from '@ngrx/signals';
+import { signalStore, type, withHooks } from '@ngrx/signals';
 import { withEntities } from '@ngrx/signals/entities';
 import { of } from 'rxjs';
 
@@ -87,6 +88,182 @@ describe('withEntitiesRemotePagination', () => {
     });
   }));
 
+  it('setEntitiesPagedResult should store entities', fakeAsync(() => {
+    TestBed.runInInjectionContext(() => {
+      const fetchEntitiesSpy = jest.fn();
+      const Store = signalStore(
+        withEntities({ entity }),
+        withCallStatus(),
+        withEntitiesRemotePagination({ entity, pageSize: 10 }),
+        withHooks(
+          ({
+            isLoading,
+            setLoaded,
+            entitiesPagedRequest,
+            setEntitiesPagedResult,
+          }) => ({
+            onInit: () => {
+              const fetchEntities = ({
+                entitiesPagedRequest,
+              }: {
+                entitiesPagedRequest: Signal<{
+                  startIndex: number;
+                  size: number;
+                  page: number;
+                }>;
+              }) => {
+                fetchEntitiesSpy(entitiesPagedRequest());
+                let result = [...mockProducts];
+                const total = result.length;
+                const options = {
+                  skip: entitiesPagedRequest()?.startIndex,
+                  take: entitiesPagedRequest()?.size,
+                };
+                if (options?.skip || options?.take) {
+                  const skip = +(options?.skip ?? 0);
+                  const take = +(options?.take ?? 0);
+                  result = result.slice(skip, skip + take);
+                }
+                return of({ entities: result, total });
+              };
+
+              effect(() => {
+                if (isLoading()) {
+                  untracked(() => {
+                    fetchEntities({
+                      entitiesPagedRequest,
+                    }).subscribe((result) => {
+                      setEntitiesPagedResult(result);
+                      setLoaded();
+                    });
+                  });
+                }
+              });
+            },
+          }),
+        ),
+      );
+
+      const store = new Store();
+      TestBed.flushEffects();
+      expect(store.entities()).toEqual([]);
+      store.setLoading();
+      jest.spyOn(store, 'setLoading');
+      tick();
+      // basic check for the first page
+      expect(store.entitiesCurrentPage().entities.length).toEqual(10);
+
+      // load a page not in cache
+      store.loadEntitiesPage({ pageIndex: 7 });
+      tick();
+      expect(fetchEntitiesSpy).toHaveBeenCalledWith({
+        startIndex: 70,
+        size: 30,
+        page: 7,
+      });
+      // check the page
+
+      expect(store.entitiesCurrentPage().entities.length).toEqual(10);
+      expect(store.entitiesCurrentPage().entities).toEqual(
+        mockProducts.slice(70, 80),
+      );
+      expect(store.entitiesCurrentPage().pageIndex).toEqual(7);
+      expect(store.entitiesCurrentPage().pageSize).toEqual(10);
+      expect(store.entitiesCurrentPage().pagesCount).toEqual(13);
+      expect(store.entitiesCurrentPage().total).toEqual(mockProducts.length);
+      expect(store.entitiesCurrentPage().hasPrevious).toEqual(true);
+      expect(store.entitiesCurrentPage().hasNext).toEqual(true);
+    });
+  }));
+
+  it('setEntitiesPagedResult with collection should store entities', fakeAsync(() => {
+    TestBed.runInInjectionContext(() => {
+      const fetchEntitiesSpy = jest.fn();
+      const collection = 'products';
+      const Store = signalStore(
+        withEntities({ entity, collection }),
+        withCallStatus({ collection }),
+        withEntitiesRemotePagination({ entity, pageSize: 10, collection }),
+        withHooks(
+          ({
+            isProductsLoading,
+            setProductsLoaded,
+            productsPagedRequest,
+            setProductsPagedResult,
+          }) => ({
+            onInit: () => {
+              const fetchEntities = ({
+                entitiesPagedRequest,
+              }: {
+                entitiesPagedRequest: Signal<{
+                  startIndex: number;
+                  size: number;
+                  page: number;
+                }>;
+              }) => {
+                fetchEntitiesSpy(entitiesPagedRequest());
+                let result = [...mockProducts];
+                const total = result.length;
+                const options = {
+                  skip: entitiesPagedRequest()?.startIndex,
+                  take: entitiesPagedRequest()?.size,
+                };
+                if (options?.skip || options?.take) {
+                  const skip = +(options?.skip ?? 0);
+                  const take = +(options?.take ?? 0);
+                  result = result.slice(skip, skip + take);
+                }
+                return of({ entities: result, total });
+              };
+
+              effect(() => {
+                if (isProductsLoading()) {
+                  untracked(() => {
+                    fetchEntities({
+                      entitiesPagedRequest: productsPagedRequest,
+                    }).subscribe((result) => {
+                      setProductsPagedResult(result);
+                      setProductsLoaded();
+                    });
+                  });
+                }
+              });
+            },
+          }),
+        ),
+      );
+
+      const store = new Store();
+      TestBed.flushEffects();
+      expect(store.productsEntities()).toEqual([]);
+      store.setProductsLoading();
+      jest.spyOn(store, 'setProductsLoading');
+      tick();
+      // basic check for the first page
+      expect(store.productsCurrentPage().entities.length).toEqual(10);
+
+      // load a page not in cache
+      store.loadProductsPage({ pageIndex: 7 });
+      tick();
+      expect(fetchEntitiesSpy).toHaveBeenCalledWith({
+        startIndex: 70,
+        size: 30,
+        page: 7,
+      });
+      // check the page
+
+      expect(store.productsCurrentPage().entities.length).toEqual(10);
+      expect(store.productsCurrentPage().entities).toEqual(
+        mockProducts.slice(70, 80),
+      );
+      expect(store.productsCurrentPage().pageIndex).toEqual(7);
+      expect(store.productsCurrentPage().pageSize).toEqual(10);
+      expect(store.productsCurrentPage().pagesCount).toEqual(13);
+      expect(store.productsCurrentPage().total).toEqual(mockProducts.length);
+      expect(store.productsCurrentPage().hasPrevious).toEqual(true);
+      expect(store.productsCurrentPage().hasNext).toEqual(true);
+    });
+  }));
   it('with collection entitiesCurrentPage should split entities in the correct pages', fakeAsync(() => {
     TestBed.runInInjectionContext(() => {
       const collection = 'products';
