902d55af645a6cff4212d7ede881e3528bff5708
Franco Basilico <franco.basilico@boxhaul.co.uk>
2024-10-12 14:08:32 +0100

    fix(fix): fix

--- apps/docs/src/app/components/navbar/navbar.component.ts
+++ apps/docs/src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,20 @@
 import { ChangeDetectionStrategy, Component, model } from '@angular/core';
 import { MatIconButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { bootstrapDiscord, bootstrapGithub, bootstrapMedium } from '@ng-icons/bootstrap-icons';
+import {
+  bootstrapDiscord,
+  bootstrapGithub,
+  bootstrapMedium,
+} from '@ng-icons/bootstrap-icons';
 import { NgIcon, provideIcons } from '@ng-icons/core';
 
 @Component({
   selector: 'docs-navbar',
   standalone: true,
   imports: [MatIconButton, MatIcon, NgIcon],
-  viewProviders: [provideIcons({ bootstrapGithub, bootstrapDiscord, bootstrapMedium })],
+  viewProviders: [
+    provideIcons({ bootstrapGithub, bootstrapDiscord, bootstrapMedium }),
+  ],
   template: ` <header
     class="fixed top-0 z-20 h-16 w-full border-b border-black/10 bg-white"
   >
@@ -22,13 +28,21 @@ import { NgIcon, provideIcons } from '@ng-icons/core';
         <mat-icon>menu</mat-icon>
       </button>
 
-      <!--        <img class="hidden h-8 w-auto md:block" src="assets/logo.svg" alt="Ngrx Traits" />-->
-      <!--        <img class="block h-8 w-auto md:hidden" src="assets/logo-small.svg" alt="Ngrx Traits" />-->
+      <img
+        class="hidden h-8 w-auto md:block"
+        src="logo.svg"
+        alt="Ngrx Traits"
+      />
+      <img
+        class="block h-8 w-auto md:hidden"
+        src="logo-small.svg"
+        alt="Ngrx Traits"
+      />
 
       <!--        <div id="docsearch"></div>-->
 
       <div class="ml-auto inline-flex gap-x-2">
-      <a
+        <a
           class="inline-flex items-center justify-center gap-x-2 rounded-lg bg-white px-3 py-2 outline-none transition-colors hover:bg-zinc-50 hover:text-zinc-800 focus-visible:text-zinc-800 focus-visible:ring-2 focus-visible:ring-blue-500 active:bg-zinc-100"
           target="_blank"
           href="https://medium.com/@gabrieldavidguerrero"
