d4c474bbef915d9c940983c0a0974ed922a4178a
Gabriel Guerrero <gabriel.issa@medivet.co.uk>
2025-01-13 06:40:25 +0000

    feat(all): upgrade to ngrx 19 , fix tests
    Upgraded all packages to to required ngrx 19 versions, fixed broken tests
    
    BREAKING CHANGE: requires ngrx 19
    
    Fix #150

--- apps/example-app/src/app/examples/signals/infinete-scroll-page/infinite-scroll-page.component.ts
+++ apps/example-app/src/app/examples/signals/infinete-scroll-page/infinite-scroll-page.component.ts
@@ -2,17 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 import { ProductsBranchDropdownComponent } from './products-branch-dropdown.component';
-import { ProductsBranchListComponent } from './products-branch-list.component';
 import { ProductsBranchStore } from './products-branch.store';
 
 @Component({
   selector: 'infinite-scroll-page',
   standalone: true,
-  imports: [
-    CommonModule,
-    ProductsBranchDropdownComponent,
-    ProductsBranchListComponent,
-  ],
+  imports: [CommonModule, ProductsBranchDropdownComponent],
   template: `<products-branch-dropdown />`,
   styles: ``,
   providers: [ProductsBranchStore],
