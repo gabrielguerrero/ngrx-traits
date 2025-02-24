fbd5c5698e4b4e706eec0b91bc0a0a1305306ab8
Gabriel Guerrero <gabriel.issa@medivet.co.uk>
2025-02-24 13:15:35 +0000

    feat(signals): add expires to withSyncWebStorage
    If the expires prop is present cached data will only be restored if is not older that the expires
    time in ms
    
    Fix #171

--- libs/ngrx-traits/signals/src/lib/with-sync-to-web-storage/with-sync-to-web-storage.spec.ts
+++ libs/ngrx-traits/signals/src/lib/with-sync-to-web-storage/with-sync-to-web-storage.spec.ts
@@ -153,6 +153,168 @@ describe('withSyncToWebStorage', () => {
       expect(store.isLoaded()).toBe(true);
     });
   }));
+
+  describe('expires', () => {
+    it('should not load from local storage if cache is expired', fakeAsync(() => {
+      const consoleWarn = jest.spyOn(console, 'warn').mockImplementation(() => {
+        /* Empty */
+      });
+      TestBed.runInInjectionContext(() => {
+        window.localStorage.setItem(
+          'test',
+          `{"entityMap":{"0":{"name":"Super Mario World","id":"0","description":"Super Nintendo Game","price":10},"1":{"name":"F-Zero","id":"1","description":"Super Nintendo Game","price":12}},"ids":["0","1"],"callStatus":"loaded"}`,
+        );
+        window.localStorage.setItem(
+          'test-date',
+          new Date(new Date().getTime() - 10000).toISOString(),
+        );
+        const Store = signalStore(
+          { protectedState: false },
+          withEntities({ entity }),
+          withCallStatus(),
+          withSyncToWebStorage({
+            key: 'test',
+            type: 'local',
+            restoreOnInit: true,
+            expires: 5000,
+            saveStateChangesAfterMs: 0,
+          }),
+        );
+        const store = new Store();
+        TestBed.flushEffects();
+        tick();
+        expect(store.entities().length).toEqual(0);
+        expect(store.isLoaded()).toBe(false);
+        expect(consoleWarn).toHaveBeenCalledWith(
+          'test local web storage expired',
+        );
+      });
+    }));
+    it('should  load from local storage if cache is not expired', fakeAsync(() => {
+      TestBed.runInInjectionContext(() => {
+        window.localStorage.setItem(
+          'test',
+          `{"entityMap":{"0":{"name":"Super Mario World","id":"0","description":"Super Nintendo Game","price":10},"1":{"name":"F-Zero","id":"1","description":"Super Nintendo Game","price":12}},"ids":["0","1"],"callStatus":"loaded"}`,
+        );
+        window.localStorage.setItem(
+          'test-date',
+          new Date(new Date().getTime() - 10000).toISOString(),
+        );
+        const Store = signalStore(
+          { protectedState: false },
+          withEntities({ entity }),
+          withCallStatus(),
+          withSyncToWebStorage({
+            key: 'test',
+            type: 'local',
+            restoreOnInit: true,
+            expires: 15000,
+            saveStateChangesAfterMs: 0,
+          }),
+        );
+        const store = new Store();
+        TestBed.flushEffects();
+        tick();
+        expect(store.entities().length).toEqual(2);
+        expect(store.isLoaded()).toBe(true);
+      });
+    }));
+
+    it('should not load from session storage if cache is expired', fakeAsync(() => {
+      const consoleWarn = jest.spyOn(console, 'warn').mockImplementation(() => {
+        /* Empty */
+      });
+      TestBed.runInInjectionContext(() => {
+        window.sessionStorage.setItem(
+          'test',
+          `{"entityMap":{"0":{"name":"Super Mario World","id":"0","description":"Super Nintendo Game","price":10},"1":{"name":"F-Zero","id":"1","description":"Super Nintendo Game","price":12}},"ids":["0","1"],"callStatus":"loaded"}`,
+        );
+        window.sessionStorage.setItem(
+          'test-date',
+          new Date(new Date().getTime() - 10000).toISOString(),
+        );
+        const Store = signalStore(
+          { protectedState: false },
+          withEntities({ entity }),
+          withCallStatus(),
+          withSyncToWebStorage({
+            key: 'test',
+            type: 'session',
+            restoreOnInit: true,
+            expires: 5000,
+            saveStateChangesAfterMs: 0,
+          }),
+        );
+        const store = new Store();
+        TestBed.flushEffects();
+        tick();
+        expect(store.entities().length).toEqual(0);
+        expect(store.isLoaded()).toBe(false);
+        expect(consoleWarn).toHaveBeenCalledWith(
+          'test local web storage expired',
+        );
+      });
+    }));
+    it('should  load from session storage if cache is not expired', fakeAsync(() => {
+      TestBed.runInInjectionContext(() => {
+        window.sessionStorage.setItem(
+          'test',
+          `{"entityMap":{"0":{"name":"Super Mario World","id":"0","description":"Super Nintendo Game","price":10},"1":{"name":"F-Zero","id":"1","description":"Super Nintendo Game","price":12}},"ids":["0","1"],"callStatus":"loaded"}`,
+        );
+        window.sessionStorage.setItem(
+          'test-date',
+          new Date(new Date().getTime() - 10000).toISOString(),
+        );
+        const Store = signalStore(
+          { protectedState: false },
+          withEntities({ entity }),
+          withCallStatus(),
+          withSyncToWebStorage({
+            key: 'test',
+            type: 'session',
+            restoreOnInit: true,
+            expires: 15000,
+            saveStateChangesAfterMs: 0,
+          }),
+        );
+        const store = new Store();
+        TestBed.flushEffects();
+        tick();
+        expect(store.entities().length).toEqual(2);
+        expect(store.isLoaded()).toBe(true);
+      });
+    }));
+
+    it('should save and load to local storage using expires', () => {
+      TestBed.runInInjectionContext(() => {
+        const Store = signalStore(
+          { protectedState: false },
+          withEntities({ entity }),
+          withCallStatus(),
+          withSyncToWebStorage({
+            key: 'test',
+            type: 'local',
+            expires: 1000,
+            restoreOnInit: false,
+            saveStateChangesAfterMs: 0,
+          }),
+        );
+        const store = new Store();
+        store.clearFromStore();
+        TestBed.flushEffects();
+        store.setLoaded();
+        patchState(store, setAllEntities(mockProducts));
+        store.saveToStorage();
+
+        store.setLoading();
+        patchState(store, setAllEntities(mockProducts.slice(0, 30)));
+
+        store.loadFromStorage();
+        expect(store.entities()).toEqual(mockProducts);
+        expect(store.isLoaded()).toBe(true);
+      });
+    });
+  });
 });
 function getFromStorage(key: string) {
   const data = window.localStorage.getItem(key);
